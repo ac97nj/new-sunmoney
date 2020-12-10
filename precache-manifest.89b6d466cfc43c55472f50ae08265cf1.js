f38ba46358face97e6f4cb0a3fa68ecf9b5c6792
ac97nj <ac97nj@gmail.com>
2020-12-10 13:00:26 +0800

    deploy

---
+++ precache-manifest.89b6d466cfc43c55472f50ae08265cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ffc08fc73df8945247",
+    "url": "/new-sunmoney/css/app.89191feb.css"
+  },
+  {
+    "revision": "f03d3a5301cf2471691b63df76c2cd75",
+    "url": "/new-sunmoney/index.html"
+  },
+  {
+    "revision": "b3ffc08fc73df8945247",
+    "url": "/new-sunmoney/js/app.839ad6b6.js"
+  },
+  {
+    "revision": "8d1c57b849ca7c612205",
+    "url": "/new-sunmoney/js/chunk-vendors.91d6648a.js"
+  },
+  {
+    "revision": "102fe772ba392b5ec84d6e293863ede0",
+    "url": "/new-sunmoney/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/new-sunmoney/robots.txt"
+  }
+]);
